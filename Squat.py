154d4bde528f4c7ab7963f6db74369ff8313ca4a
Byungjun Seo <94103653+Byungjun97@users.noreply.github.com>
2022-11-18 16:14:48 +0900

    Add files via upload

--- Squat.py
+++ Squat.py
@@ -31,20 +31,20 @@ def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
     c = np.array(c) # End
-    
+
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
-    
+
     if angle >180.0:
         angle = 360-angle
-        
+
     return angle
 # menu func
 def menu_status(status):
     match status:
         case 0: #nothing
             return " -"
-        case 1: #덤벨
+        case 1: #Dumbell
             return "Dumbell"
         case 2: #lunge
             return "lunge"
@@ -56,13 +56,13 @@ def menu_status(status):
             return "Exit"
         case _: #default
             return "--"
-        
+
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
 
 while(menu_num != 5):
-    
+
 
     # Select menu with hand
     with mp_hands.Hands(
@@ -125,13 +125,13 @@ while(menu_num != 5):
 
         # Display menu
         cv2.rectangle(image, (0,0), (400,73), (185,245,16), -1)
-        
+
         cv2.putText(image, 'Select', (20,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
         cv2.putText(image, str(fingerCount), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-            
+
         cv2.putText(image, 'Menu', (200,12),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
         cv2.putText(image, str(menu_status(fingerCount)),(150,60),cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-            
+
         menu_num_temp1 = fingerCount
         menu_num_count += 1
         if(menu_num_count >= menu_time) :
@@ -143,138 +143,130 @@ while(menu_num != 5):
             menu_num_temp1 = 0
             menu_num_temp2 = 0
             menu_num_count = 0
-            
-        
+
+
         print(menu_num)
-        
+
         # Display image
         cv2.namedWindow('MediaPipe Menu')
         cv2.imshow('MediaPipe Menu', image)
         cv2.moveWindow('MediaPipe Menu', window_pos_x, window_pos_y)
-        
+
         if (cv2.waitKey(5) & 0xFF == 27) or (menu_num != 0):
           #cv2.destroyAllWindows()
           menu_num_count = 0
           break
-      
-      
-        
-    if (menu_num == 1) :
-       print("Menu 1")
-       cv2.imshow('MediaPipe Menu', image)
 
+
+
+    if (menu_num == 1) :
+        print("Menu 1")
+        cv2.imshow('MediaPipe Menu', image)
 
     if (menu_num == 2) :
         print("Menu 2")
         cv2.imshow('MediaPipe Menu', image)
 
     if (menu_num == 3) :
-        print("Menu 3")
+        print("Menu 3")      
         #cap = cv2.VideoCapture(0)
 
-        with mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose :
-              while cap.isOpened() :
-                  ret, frame = cap.read()
+      with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
 
-                  # Recolor image to RGB
-                  image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                  image.flags.writeable = False
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
-                  # Make detection
-                  results = pose.process(image)
+            # Make detection
+            results = pose.process(image)
 
-                  # Recolor back to BGR
-                  image.flags.writeable = True
-                  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-                  # Extract landmarks
-                  try :
-                  landmarks = results.pose_landmarks.landmark
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
 
-                  # Get coordinates
-                  lshoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                  lelbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                  lwrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                # Get coordinates
+                lshoulder= [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                lelbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                lwrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-                  rshoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-                  relbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-                  rwrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                rshoulder= [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                relbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                rwrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
 
-                  # Calculate angle
-                  langle = int(calculate_angle(lshoulder, lelbow, lwrist))
-                  rangle = int(calculate_angle(rshoulder, relbow, rwrist))
-                  # Visualize angle
-                  cv2.putText(image, str(langle),
-                      tuple(np.multiply(lelbow, [640, 480]).astype(int)),
-                      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                  )
+                # Calculate angle
+                langle = int(calculate_angle(lshoulder, lelbow, lwrist))
+                rangle = int(calculate_angle(rshoulder, relbow, rwrist))
+                # Visualize angle
+                cv2.putText(image, str(langle), 
+                               tuple(np.multiply(lelbow, [640, 480]).astype(int)), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
 
-                  cv2.putText(image, str(rangle),
-                      tuple(np.multiply(relbow, [640, 480]).astype(int)),
-                      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                  )
+                cv2.putText(image, str(rangle), 
+                               tuple(np.multiply(relbow, [640, 480]).astype(int)), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                    )
 
-                  # Squat counter logic
+                # Curl counter logic
+                if (langle < 100 or rangle < 100)
+                    stage = "down"
+                if (langle > 150 or rangle > 150 and stage =='down'):
+                    stage = "up"
+                    counter +=1
+                    print(counter)
 
-                  if (langle < 100 or rangle < 100)
-                      stage = "down"
-                
-                  if (langle > 150 or langle > 150 and stage =='down'):
-                      stage = "up"
-                      counter += 1
-                      print(counter)
-               
+            except:
+                pass
 
+            # Setup status box
+            cv2.rectangle(image, (0,0), (350,73), (185,245,16), -1)
 
-                              except :
-                              pass
+            # Rep data
+            cv2.putText(image, 'Count', (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (10,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
 
-                              # Setup status box
-                              cv2.rectangle(image, (0, 0), (350, 73), (185, 245, 16), -1)
-
-                              # Rep data
-                              cv2.putText(image, 'Count', (15, 12),
-                                  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                              cv2.putText(image, str(counter),
-                                  (10, 60),
-                                  cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-                              # Stage data
-                              cv2.putText(image, 'STAGE', (160, 12),
-                                  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                              cv2.putText(image, stage,
-                                  (100, 60),
-                                  cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            # Stage data
+            cv2.putText(image, 'STAGE', (160,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (100,60), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
 
 
-                              # Render detections
-                              mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color = (245, 117, 66), thickness = 2, circle_radius = 2),
-                                  mp_drawing.DrawingSpec(color = (245, 66, 230), thickness = 2, circle_radius = 2)
-                              )
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                     )               
 
-                              cv2.imshow('MediaPipe Menu', image)
-                              cv2.moveWindow('MediaPipe Menu', window_pos_x, window_pos_y)
+            cv2.imshow('MediaPipe Menu', image)
+            cv2.moveWindow('MediaPipe Menu',  window_pos_x, window_pos_y)
 
-                              if cv2.waitKey(10) & 0xFF == ord('r') : #pose로 수정필요
-                                  counter = 0
-                                  lcounter = 0
-                                  rcounter = 0
-                                  stage = "Reset"
-                                  if cv2.waitKey(10) & 0xFF == 27:
-              menu_num = 0
-                  break
-
-
+            if cv2.waitKey(10) & 0xFF == ord('r'): #pose로 수정필요 
+                counter = 0
+                lcounter = 0
+                rcounter = 0
+                stage = "Reset"
+            if cv2.waitKey(10) & 0xFF == 27:
+                menu_num=0
+                break
     if (menu_num == 4) :
         print("Menu 4")
         cv2.imshow('MediaPipe Menu', image)
 
-        
+
 
 ############################################
-        
+
 #cap.release()
 #cv2.destroyAllWindows()
 #execute magic mirror
-        
